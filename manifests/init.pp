ee15fb9c52b5bcca7a692349c7b2fe38c665afc7
Scott Smerchek <scott.smerchek@softekinc.com>
2012-11-07 10:47:43 -0600

    Added class documentation and fixed puppet-lint issues.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,25 @@
+# Class: java7
+#
+# This module manages Oracle Java7
+# Parameters: none
+# Requires:
+#  apt
+# Sample Usage:
+#  include java7
 class java7 {
-   include apt
-  
+  include apt
+
   apt::ppa { 'ppa:webupd8team/java': }
-  file { "/tmp/java.preseed":
+  file { '/tmp/java.preseed':
     source => 'puppet:///modules/java7/java.preseed',
-    mode => 600,
+    mode   => '0600',
     backup => false,
   }
   package { 'oracle-java7-installer':
     responsefile => '/tmp/java.preseed',
-    require      => [Apt::Ppa['ppa:webupd8team/java'],File['/tmp/java.preseed']],
+    require      => [
+                      Apt::Ppa['ppa:webupd8team/java'],
+                      File['/tmp/java.preseed']
+                    ],
   }
 }
