498f50e7bbcddb7d85e3b3835e0e10276d61985d
Scott Smerchek <scott.smerchek@softekinc.com>
2012-11-07 13:20:32 -0600

    Added operating system check to be explicit about supported operating systems.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,19 +7,24 @@
 # Sample Usage:
 #  include java7
 class java7 {
-  include apt
+  case $::operatingsystem {
+    debian, ubuntu: {
+      include apt
 
-  apt::ppa { 'ppa:webupd8team/java': }
-  file { '/tmp/java.preseed':
-    source => 'puppet:///modules/java7/java.preseed',
-    mode   => '0600',
-    backup => false,
-  }
-  package { 'oracle-java7-installer':
-    responsefile => '/tmp/java.preseed',
-    require      => [
-                      Apt::Ppa['ppa:webupd8team/java'],
-                      File['/tmp/java.preseed']
-                    ],
+      apt::ppa { 'ppa:webupd8team/java': }
+      file { '/tmp/java.preseed':
+        source => 'puppet:///modules/java7/java.preseed',
+        mode   => '0600',
+        backup => false,
+      }
+      package { 'oracle-java7-installer':
+        responsefile => '/tmp/java.preseed',
+        require      => [
+                          Apt::Ppa['ppa:webupd8team/java'],
+                          File['/tmp/java.preseed']
+                        ],
+      }
+    }
+    default: { notice "Unsupported operatingsystem ${::operatingsystem}" }
   }
 }
